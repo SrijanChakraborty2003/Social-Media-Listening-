fcb8d421e58f1e0c857969596142da9713a0d728
SrijanChakraborty2003 <166740425+SrijanChakraborty2003@users.noreply.github.com>
2025-03-27 11:00:52 +0530

    Update Social Media Listening.py

--- Social Media Listening.py
+++ Social Media Listening.py
@@ -2,7 +2,6 @@ import streamlit as st
 import joblib
 import re
 import string
-
 def clean_text(text):
     text = text.lower()
     text = re.sub(r'\[.*?\]', '', text)
@@ -10,22 +9,16 @@ def clean_text(text):
     text = re.sub(r'\w*\d\w*', '', text)
     text = text.translate(str.maketrans('', '', string.punctuation))
     return text
-
-# Load saved model and vectorizer
 vectorizer = joblib.load('tfidf_vectorizer (3).pkl')
 svm_model = joblib.load('emotion_classification_model.pkl')
-
 def predict_emotion(text):
     cleaned_text = clean_text(text)
     transformed_text = vectorizer.transform([cleaned_text])
     prediction = svm_model.predict(transformed_text)
     emotions = {0: 'Sadness', 1: 'Joy', 2: 'Love', 3: 'Anger', 4: 'Fear', 5: 'Surprise'}
     return emotions.get(prediction[0], 'Unknown')
-
-# Streamlit UI
 st.title("Emotion Detection App")
 st.write("Enter a text message to predict its emotion.")
-
 user_input = st.text_area("Enter your message here:")
 if st.button("Predict Emotion"):
     if user_input.strip():
